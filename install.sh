004a6e15ee2689c9d0717dc40930befe894631cb
Olliv3r <oliveobom100@gmail.com>
2019-03-12 02:21:07 -0400

    %

--- install.sh
+++ install.sh
@@ -3,13 +3,14 @@
 apt update
 apt upgrade
 apt install nmap python
-pip install python-nmap
+pip3.7 install python-nmap
 
 if test -d /usr/bin;then
 	cat Nscan.py >> /usr/bin/Nscan
 	chmod 777 /usr/bin/Nscan
 elif test -d $PREFIX/bin;then
 	cat Nscan.py >> $PREFIX/bin/Nscan
+	chmod 777 $PREFIX/bin/Nscan
 	sleep 0.5
 	echo "Run 'Nscan'"
 fi
