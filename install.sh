cc13aad9a126e3a04f3c05cb2f5096e46087e0e4
Olliv3r <oliveobom100@gmail.com>
2019-11-02 02:38:00 -0400

    $:

--- install.sh
+++ install.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
+clear
+tput cup 0
+tput civis
+echo -e "\r\033[0mInstalling...[\033[01;93mNone\033[0m]"
+(apt update && apt upgrade -y && apt install nmap python -y && pip  install python-nmap) &> /dev/null
+tput cup 0
+echo -e "\r\033[0mInstalling...[\033[01;93mDone\033[0m]"
 
-apt update
-apt upgrade
-apt install nmap python
-pip3.7 install python-nmap
-
-
-echo -e "\033[01;92m./Nscan py"
+echo -e "\033[01;92mpython3 ./Nscan py"
+tput cnorm
